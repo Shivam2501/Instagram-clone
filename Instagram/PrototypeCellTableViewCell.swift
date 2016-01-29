2bd30d183bacd7eaa7ef9a1ecd02a32bd6dea9a0
Shivam Bharuka <shivam.bharuka@gmail.com>
2016-01-28 19:49:27 -0600

    image

---
+++ Instagram/PrototypeCellTableViewCell.swift
@@ -0,0 +1,26 @@
+//
+//  PrototypeCellTableViewCell.swift
+//  Instagram
+//
+//  Created by Shivam Bharuka on 1/28/16.
+//  Copyright © 2016 Shivam Bharuka. All rights reserved.
+//
+
+import UIKit
+
+class PrototypeCellTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var postImage: UIImageView!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
--- Instagram/ViewController.swift
+++ Instagram/ViewController.swift
@@ -7,12 +7,43 @@
 //
 
 import UIKit
+import AFNetworking
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate{
+    
+    @IBOutlet weak var tableView: UITableView!
+    var photos: [NSDictionary]?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        tableView.dataSource = self
+        tableView.delegate = self
+        tableView.rowHeight = 320;
+        
         // Do any additional setup after loading the view, typically from a nib.
+        let clientId = "e05c462ebd86446ea48a5af73769b602"
+        let url = NSURL(string:"https://api.instagram.com/v1/media/popular?client_id=\(clientId)")
+        let request = NSURLRequest(URL: url!)
+        let session = NSURLSession(
+            configuration: NSURLSessionConfiguration.defaultSessionConfiguration(),
+            delegate:nil,
+            delegateQueue:NSOperationQueue.mainQueue()
+        )
+        
+        let task : NSURLSessionDataTask = session.dataTaskWithRequest(request,
+            completionHandler: { (dataOrNil, response, error) in
+                if let data = dataOrNil {
+                    if let responseDictionary = try! NSJSONSerialization.JSONObjectWithData(
+                        data, options:[]) as? NSDictionary {
+                            NSLog("response: \(responseDictionary)")
+                            self.photos = responseDictionary["data"] as! [NSDictionary]
+                            
+                            self.tableView.reloadData()
+                    }
+                }
+        });
+        task.resume()
+        
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +51,27 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCellWithIdentifier("PrototypeCell", forIndexPath: indexPath) as! PrototypeCellTableViewCell
+        let photo = photos![indexPath.row]
+        let posterPath = photo["images"] as! NSDictionary
+        let imageURL = posterPath["standard_resolution"] as! NSDictionary
+        let URL_String = imageURL["url"] as! String
+        let URL = NSURL(string: URL_String)
+
+        cell.postImage.setImageWithURL(URL!)
+        return cell
+    }
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        if let photos = photos{
+             print("row \(photos.count)")
+            return photos.count
+        }else{
+            return 0
+        }
+    }
 
 }
 
